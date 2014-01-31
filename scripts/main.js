f439660e1ab96e606ee2fca626f563669cc65516
Jeremy Kahn <jeremyckahn@gmail.com>
2014-01-30 22:32:14 -0600

    Adds a bounce animation.

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,30 @@ require.config({
   }
 });
 
-require(['rekapi'], function (rekapi) {
- console.log(rekapi)
+require(['rekapi'], function (Rekapi) {
+  function toArray (arrayLike) {
+    return Array.prototype.slice.call(arrayLike);
+  }
+
+  function $ () {
+    return toArray(
+        document.querySelectorAll.apply(document, arguments));
+  }
+
+  var rekapi = new Rekapi(document.body);
+  var lis = $('li');
+
+  lis.forEach(function (li) {
+    var durationVariance = Math.random() * 250;
+    var actor = rekapi.addActor({ context: li });
+    actor
+      .keyframe(0, {
+        transform: 'translateZ(1px)'
+      }).keyframe(1000 + durationVariance, {
+        transform: 'translateZ(0px)'
+      }, 'bouncePast')
+      .wait(2500);
+  });
+
+  rekapi.play();
 });
